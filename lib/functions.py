491ba46b89a3ec61e8d8cd6d09d09ba05d780563
kurui <kuruijoshua@gmail.com>
2025-08-13 13:49:51 +0300

    Implement greet, greet_with_default, add, and halve functions in Python

--- lib/functions.py
+++ lib/functions.py
@@ -1,22 +1,17 @@
-# lib/functions.py
 
-# 1. greet_programmer()
+
 def greet_programmer():
     print("Hello, programmer!")
 
-# 2. greet(name)
 def greet(name):
     print(f"Hello, {name}!")
 
-# 3. greet_with_default(name="programmer")
 def greet_with_default(name="programmer"):
     print(f"Hello, {name}!")
 
-# 4. add(num1, num2)
 def add(num1, num2):
     return num1 + num2
 
-# 5. halve(number)
 def halve(number):
     if not isinstance(number, (int, float)):
         return None
